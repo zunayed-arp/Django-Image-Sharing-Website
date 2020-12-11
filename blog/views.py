1b7a821886dc3ccd71febd7512493298d3708173
zunayed <zunayed.arp@gmail.com>
2020-12-11 22:27:15 +0600

    after global config and added templates

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'zunayed',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'December 11,2020'
+    },
+    {
+        'author': 'Am Raul',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'December 12,2020'
+    }
+]
+
 
 def home(request):
-    #return HttpResponse('<h1> Blog Home</h1>')
-    return render(request,'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 
 def about(request):
+
     return render(request,'blog/about.html')
     #return HttpResponse('<h1> Blog About</h1>')
 
