0100be4b31912b266515a3392535957a2217d2ec
zunayed <zunayed.arp@gmail.com>
2020-12-17 03:47:03 +0600

    end of 4th chapter

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'zunayed',
-        'title': 'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'December 11,2020'
-    },
-    {
-        'author': 'Am Raul',
-        'title': 'Blog Post 2',
-        'content': 'First post content',
-        'date_posted': 'December 12,2020'
-    }
-]
+from .models import  Post
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
