d22adb59e7f1ddcf48b67369f3538d7a4947ed59
zunayed-arp <zunayed.arp@gmail.com>
2022-09-23 11:55:30 +0600

    user creation ,updte form

--- feed/forms.py
+++ feed/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from .models import Comments,Post
-
+from .models import Comments, Post
 
 
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['description','pic','tags']
+        fields = ['description', 'pic', 'tags']
+
+
 class NewCommentForm(forms.ModelForm):
     class Meta:
         model = Comments
